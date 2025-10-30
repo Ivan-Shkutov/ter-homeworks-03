470144704062f1eb7a163491bb44ce044ebbaddf
Ivan-Shkutov <157962727+Ivan-Shkutov@users.noreply.github.com>
2025-10-30 20:01:33 +0300

    Update ansible.tf

--- src/ansible.tf
+++ src/ansible.tf
@@ -1,28 +1,12 @@
 # Создаём inventory-файл для Ansible
 resource "local_file" "ansible_inventory" {
-  filename = "${path.module}/inventory.ini"
+  content = templatefile("${path.module}/all_hosts.tftpl",
 
-  content = templatefile("${path.module}/templates/inventory.tmpl", {
-    webservers = [
-      for vm in yandex_compute_instance.web : {
-        name        = vm.name
-        external_ip = vm.network_interface[0].nat_ip_address
-        fqdn        = vm.fqdn
-      }
-    ]
-    databases = [
-      for vm in yandex_compute_instance.db : {
-        name        = vm.name
-        external_ip = vm.network_interface[0].nat_ip_address
-        fqdn        = vm.fqdn
-      }
-    ]
-    storage = [
-      for vm in yandex_compute_instance.storage : {
-        name        = vm.name
-        external_ip = vm.network_interface[0].nat_ip_address
-        fqdn        = vm.fqdn
-      }
-    ]
+    {
+    webservers = yandex_compute_instance.web
+    databases  = yandex_compute_instance.main_replica
+    storage    = [yandex_compute_instance.storage]
+
   })
+  filename = "${abspath(path.module)}/all_hosts.cfg"
 }
